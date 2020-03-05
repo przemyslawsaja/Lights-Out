d1aeb1c162367ee3d50a6f5ef6c588484cf81381
przemyslawsaja <przemyslawsaja@gmail.com>
2020-03-05 12:02:47 +0100

    Updates

---
+++ precache-manifest.6afad38dcbca5be8b37587d31442cb69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dee89aa5962ffa5f6f94bab591f6948",
+    "url": "/przemyslawsaja/Lights_Out/index.html"
+  },
+  {
+    "revision": "fef14322b4f40f4938ad",
+    "url": "/przemyslawsaja/Lights_Out/static/css/main.39a10ec8.chunk.css"
+  },
+  {
+    "revision": "cd632c10d71aea36997b",
+    "url": "/przemyslawsaja/Lights_Out/static/js/2.50b6cd0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/przemyslawsaja/Lights_Out/static/js/2.50b6cd0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef14322b4f40f4938ad",
+    "url": "/przemyslawsaja/Lights_Out/static/js/main.56b4975f.chunk.js"
+  },
+  {
+    "revision": "f8e0b321cb8b5b2515c2",
+    "url": "/przemyslawsaja/Lights_Out/static/js/runtime-main.e74db2e9.js"
+  }
+]);
